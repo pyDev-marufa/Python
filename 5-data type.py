a87b0f32258ef7ce264f416dc51336ecdae7ec3d
pyDev-marufa <myselfasha@outlook.com>
2024-12-25 12:06:23 +0600

    Add files via upload

---
+++ 5-data type.py
@@ -0,0 +1,14 @@
+#subscripting
+print("Hello[0]")
+
+#sreing
+print("123" + "456")
+
+#integer = whole
+print (123 + 123)
+
+# python e large integer bujhate comma er alternative _ underscore
+print(123_456_667)
+
+#Float
+#boolen
