c25866621b22291220aa639a9976f6ede4ac2c79
pyDev-marufa <myselfasha@outlook.com>
2024-12-26 18:09:39 +0600

    Add files via upload

---
+++ 7-mathmatical operators.py
@@ -0,0 +1,22 @@
+print("My age: " + str(12))
+print(12 + 34)
+print(7 - 3)
+print(3 * 4)
+print(6 / 3)  #implicit typecast : answer is 2.0 : float
+print(6 // 3) #devides cleanly & wipes out all decimal places : int
+print(2 ** 3) #exponent. to the power
+
+
+#more than one operators in the same line of code
+
+# PEMDASLR  left to right
+
+
+# ()
+# **
+# * OR /
+# + or -
+
+print(3 * 3 + 3 / 3 - 3)
+
+print(3 * (3 + 3) / 3 - 3)
